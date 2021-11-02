18d5fc78f67128faaa70227ec6ae339a27fbb1fa
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-11-02 13:56:47 -0600

    added git ignore, updated compress

--- src/compress.c
+++ src/compress.c
@@ -1,13 +1,19 @@
 #include "algx.h"
+#include "gzip_codec.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
+    // adding integers
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
 
     int r = alg_add(a, b);
-    printf("%d+%d=%d\n", a, b, r);
+     printf("%d+%d=%d\n", a, b, r);
+    
+    // gzip compression
+    compressWithGzip();
 
+    
 }
