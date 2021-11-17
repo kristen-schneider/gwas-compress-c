95b81fc2f749f25f64aef043b9eb377d0c9107b9
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-11-17 16:35:25 -0700

    running gzip_codec.c works. can deflate piping data to zcat.

--- src/gzip_codec.c
+++ src/gzip_codec.c
@@ -1,70 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <zlib.h>
 #include "gzip_codec.h"
 
-// CHUNK is the size of the memory chunk used by the zlib routines.
 #define CHUNK 0x4000
+
+#define CALL_ZLIB(x) {                                                  \
+        int status;                                                     \
+        status = x;                                                     \
+        if (status < 0) {                                               \
+            fprintf (stderr,                                            \
+                     "%s:%d: %s returned a bad status of %d.\n",        \
+                     __FILE__, __LINE__, #x, status);                   \
+            exit (EXIT_FAILURE);                                        \
+        }                                                               \
+    }
+
 #define windowBits 15
-#define ENABLE_ZLIB_GZIP 32
+#define GZIP_ENCODING 16
 
-#define CALL_ZLIB(x) {  \
-    int status;     \
-    status = x;     \
-    if (status < 0) \
-    {               \
-            fprintf(stderr, "%s:%d: %s returned a bad status of %d.\n", __FILE__, __LINE__, #x, status);  \
-            exit(EXIT_FAILURE);\
-    }              \
- }                 \
-
-
-
-
-int compressWithGzip()
+static void strm_init (z_stream * strm)
 {
-    const char * file_name = "test.gz";
-    FILE * file;
-    z_stream strm = {0};
-    unsigned char in[CHUNK];
+    strm->zalloc = Z_NULL;
+    strm->zfree  = Z_NULL;
+    strm->opaque = Z_NULL;
+    CALL_ZLIB (deflateInit2 (strm, Z_DEFAULT_COMPRESSION, Z_DEFLATED,
+                             windowBits | GZIP_ENCODING, 8,
+                             Z_DEFAULT_STRATEGY));
+}
+
+
+static const char * message = 
+"Shall I compare thee to a summer's day?\n"
+"Thou art more lovely and more temperate:\n"
+"Rough winds do shake the darling buds of May,\n"
+"And summer's lease hath all too short a date;\n"
+;
+
+int main ()
+{
     unsigned char out[CHUNK];
-
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.next_in = in;
-    strm.avail_in = 0;
-    CALL_ZLIB (inflateInit2 (& strm, windowBits | ENABLE_ZLIB_GZIP));
-
-    /* Open the file. */
-
-    file = fopen (file_name, "rb");
-
-    //while (1)
-    //{
-    //    int bytes_read;
-
-    //    bytes_read = fread (in, sizeof (char), sizeof (in), file);
-
-    //    strm.avail_in = bytes_read;
-    //    do
-    //    {
-    //        unsigned have;
-    //        strm.avail_out = CHUNK;
-    //        strm.next_out = out;
-    //        CALL_ZLIB (inflate (& strm, Z_NO_FLUSH));
-    //        have = CHUNK - strm.avail_out;
-    //        fwrite (out, sizeof (unsigned char), have, stdout);
-    //    }
-    //    while (strm.avail_out == 0);
-    //    if (feof (file))
-    //    {
-    //        inflateEnd (& strm);
-    //        break;
-    //    }
-    //}
+    z_stream strm;
+    strm_init (& strm);
+    strm.next_in = (unsigned char *) message;
+    strm.avail_in = strlen (message);
+    do {
+        int have;
+        strm.avail_out = CHUNK;
+        strm.next_out = out;
+        CALL_ZLIB (deflate (& strm, Z_FINISH));
+        have = CHUNK - strm.avail_out;
+        fwrite (out, sizeof (char), have, stdout);
+    }
+    while (strm.avail_out == 0);
+    deflateEnd (& strm);
 
     return 0;
 }
