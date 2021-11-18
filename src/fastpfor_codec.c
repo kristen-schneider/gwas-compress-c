c6cc8ff323a9e7df50775a5415090a5d3635c27a
Kristen Schneider <krsc0813@mendel.colorado.edu>
2021-11-17 18:51:58 -0700

    added htslib functionality. using namespace error

--- src/fastpfor_codec.c
+++ src/fastpfor_codec.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
 #include "fastpfor_codec.h"
+#include <htslib/vcf.h>
 //#include "/home/krsc0813/projects/gwas-compress-c/lib/FastPFor/headers/fastpfor.h"
 
-int compress_with_fastpfor()
-{
+//#include "FastPFor/headers/codecfactory.h"
+//#include "FastPFor/headers/deltautil.h"
+
+int main() {
+    IntegerCODEC &codec = *CODECFactory::getFromName("simdfastpfor256");
+}
+
+int compress_with_fastpfor(){   
     return 0;
 }
 
